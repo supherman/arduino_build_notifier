a60e24d4d39c68e383bcd82a09e393964fcca83a
Herman Moreno <herman.moreno@crowdint.com>
2012-11-09 17:07:08 -0600

    The building indicator now blinks

--- lib/arduino_build_notifier/arduino/indicators/building.rb
+++ lib/arduino_build_notifier/arduino/indicators/building.rb
@@ -1,8 +1,23 @@
+require 'celluloid'
+
 module ArduinoBuildNotifier
   module Arduino
     module Indicators
       class Building < Base
+        include Celluloid
 
+        def on
+          @timer = every 1 do
+            @led.on
+            sleep 0.5
+            @led.off
+          end
+        end
+
+        def off
+          @led.off
+          @timer.cancel if @timer
+        end
       end
     end
   end
